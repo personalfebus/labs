e050fcb51d064da6ee5331a3d578ae59b15519d9
personalfebus <gleb89206535210@gmail.com>
2021-06-19 16:55:35 +0300

    added print

--- code_gen/lab4/bb_node.cpp
+++ code_gen/lab4/bb_node.cpp
@@ -30,8 +30,8 @@ void bb_node::add_edge(int node, int desc) {
 }
 
 bb_graph::bb_graph(FunctionAST *f) {
-    unique_ptr<bb_node> uniquePtr = make_unique<bb_node>("skip 0");
-    unique_ptr<bb_node> uniquePtr1 = make_unique<bb_node>("return 1");
+    unique_ptr<bb_node> uniquePtr = make_unique<bb_node>("skip_0");
+    unique_ptr<bb_node> uniquePtr1 = make_unique<bb_node>("return_1");
     nodes.push_back(move(uniquePtr));
     nodes.push_back(move(uniquePtr1));
     //to do
@@ -53,8 +53,8 @@ bb_graph::bb_graph(FunctionAST *f) {
     for (int i = 0; i < f->body.size(); i++) {
         if (i == f->body.size() - 1) {
             //return stmt
-            string skip_str = "local_return ";
-            skip_str += nodes.size();
+            string skip_str = "local_return_";
+            skip_str += to_string(nodes.size());
             unique_ptr<bb_node> nodePtr = make_unique<bb_node>(skip_str);
             nodePtr->add_edge(1, 1);
             nodes[1]->add_edge(nodes.size(), 0);
@@ -74,8 +74,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 ExprAST *ex = f->body[i].release();
                 IfExprAST *exif = (IfExprAST*)ex;
 
-                string cond_str = "condition ";
-                cond_str += nodes.size();
+                string cond_str = "condition_";
+                cond_str += to_string(nodes.size());
                 unique_ptr<bb_node> condPtr = make_unique<bb_node>(cond_str);
                 condPtr->add_expr(move(exif->Cond));
                 nodes.emplace_back(move(condPtr));
@@ -83,8 +83,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 nodes[nodes.size() - 1]->add_edge(nodes.size(), 1); //then
                 nodes[nodes.size() - 1]->add_edge(nodes.size() + 1, 1); //else
 
-                string then_str = "then ";
-                then_str += nodes.size();
+                string then_str = "then_";
+                then_str += to_string(nodes.size());
                 unique_ptr<bb_node> thenPtr = make_unique<bb_node>(then_str);
 //                thenPtr->exprs = exif->Then;
                 for (int j = 0; j < exif->Then.size(); j++) {
@@ -95,8 +95,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 nodes[nodes.size() - 1]->add_edge(nodes.size() - 2, 0);
                 nodes[nodes.size() - 1]->add_edge(nodes.size() + 1, 1);
 
-                string else_str = "else ";
-                else_str += nodes.size();
+                string else_str = "else_";
+                else_str += to_string(nodes.size());
                 unique_ptr<bb_node> elsePtr = make_unique<bb_node>(else_str);
                 //elsePtr->exprs = exif->Else;
                 for (int j = 0; j < exif->Else.size(); j++) {
@@ -113,15 +113,15 @@ bb_graph::bb_graph(FunctionAST *f) {
                 ExprAST *ex = f->body[i].release();
                 ForExprAST *exfor = (ForExprAST*)ex;
 
-                string prev_str = "for_previous ";
-                prev_str += nodes.size();
+                string prev_str = "for_previous_";
+                prev_str += to_string(nodes.size());
                 unique_ptr<bb_node> prevPtr = make_unique<bb_node>(prev_str);
                 prevPtr->add_expr(move(exfor->Start));
                 nodes.emplace_back(move(prevPtr));
                 nodes[nodes.size() - 1]->add_edge(nodes.size(), 1);
 
-                string cond_str = "for_condition ";
-                cond_str += nodes.size();
+                string cond_str = "for_condition_";
+                cond_str += to_string(nodes.size());
                 unique_ptr<bb_node> condPtr = make_unique<bb_node>(cond_str);
                 condPtr->add_expr(move(exfor->End));
                 nodes.emplace_back(move(condPtr));
@@ -134,8 +134,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 //body --> step
                 //step --> cond
 
-                string body_str = "for_body ";
-                body_str += nodes.size();
+                string body_str = "for_body_";
+                body_str += to_string(nodes.size());
                 unique_ptr<bb_node> bodyPtr = make_unique<bb_node>(body_str);
                 for (int j = 0; j < exfor->Body.size(); j++) {
                     // rec
@@ -145,8 +145,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 nodes[nodes.size() - 1]->add_edge(nodes.size() - 2, 0);
                 nodes[nodes.size() - 1]->add_edge(nodes.size(), 1);
 
-                string step_str = "for_step ";
-                step_str += nodes.size();
+                string step_str = "for_step_";
+                step_str += to_string(nodes.size());
                 unique_ptr<bb_node> stepPtr = make_unique<bb_node>(step_str);
                 stepPtr->add_expr(move(exfor->Step));
                 nodes.emplace_back(move(stepPtr));
@@ -157,8 +157,8 @@ bb_graph::bb_graph(FunctionAST *f) {
                 need_new_block = true;
             } else {
                 if (need_new_block) {
-                    string skip_str = "skip ";
-                    skip_str += nodes.size();
+                    string skip_str = "skip_";
+                    skip_str += to_string(nodes.size());
                     unique_ptr<bb_node> nodePtr = make_unique<bb_node>(skip_str);
                     nodes.emplace_back(move(nodePtr));
                 }
@@ -171,4 +171,22 @@ bb_graph::bb_graph(FunctionAST *f) {
 
 void bb_graph::add_node(unique_ptr<bb_node> node) {
     nodes.emplace_back(move(node));
+}
+
+void bb_graph::print_graph() {
+    cout << nodes.size() << endl;
+    printf("\ndigraph {\n");
+
+    for (int i = 0; i < nodes.size(); i++) {
+        printf("%s[shape=square];\n", nodes[i]->label.c_str());
+    }
+
+    for (int i = 0; i < nodes.size(); i++) {
+        for (int j = 0; j < nodes[i]->out_edges.size(); j++) {
+            string tmp = nodes[nodes[i]->out_edges[j]]->label;
+            printf("%s -> %s;\n", nodes[i]->label.c_str(), tmp.c_str());
+        }
+    }
+
+    printf("}");
 }
